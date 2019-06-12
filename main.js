79ac3b26cdb2e3f928b1b00099f967f756a02b1d
Mommygohi <46762402+Mommygohi@users.noreply.github.com>
2019-06-12 12:44:36 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,1 @@
-function process(value){
-  if(value == "clear"){
-    document.getElementById("output").innerText = "";
-  } else if(value == "submit"){
-    calculate();
-  } else {
-    document.getElementById("output").innerText += value;
-  }
-}
 
-function calculate(){
-  document.getElementById("output").innerText = eval(document.getElementById("output").innerText);
-}
